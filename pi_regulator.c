6ecd50acd24b9007196509789be2ae050a48508b
greglcrx <71424335+greglcrx@users.noreply.github.com>
2021-04-29 12:02:55 +0200

    PI correction

--- pi_regulator.c
+++ pi_regulator.c
@@ -10,14 +10,17 @@
 #include <sensors\VL53L0X\VL53L0X.h>
 #include <pi_regulator.h>
 #include <process_image.h>
+#include <leds.h>
 
 //#define MOTOR_SPEED_LIMIT   13 // [cm/s]
 #define NSTEP_ONE_TURN      1000 // number of step for 1 turn of the motor
 #define WHEEL_PERIMETER     13 // [cm]
+
 #define PI                  3.1415926536f
 //TO ADJUST IF NECESSARY. NOT ALL THE E-PUCK2 HAVE EXACTLY THE SAME WHEEL DISTANCE
 #define WHEEL_DISTANCE      5.35f    //cm
 #define PERIMETER_EPUCK     (PI * WHEEL_DISTANCE)
+#define MM_TO_CM			0.1
 
 #define POSITION_REACHED 1
 #define POSITION_NOT_REACHED 0
@@ -32,13 +35,12 @@ static int32_t position_to_reach_left=0;
 //simple PI regulator implementation
 int16_t pi_regulator(uint16_t distance, uint16_t goal){
 
-	uint16_t error = 0;
-	uint16_t speed = 0;
+	int16_t error = 0;
+	int32_t speed = 0;
 
-	static uint16_t sum_error = 0;
+	int16_t sum_error = 0;
 
 	error = distance-goal;
-
 	//disables the PI regulator if the error is to small
 	//this avoids to always move as we cannot exactly be where we want and 
 	//the camera is a bit noisy
@@ -49,18 +51,20 @@ int16_t pi_regulator(uint16_t distance, uint16_t goal){
 	sum_error += error;
 
 	//we set a maximum and a minimum for the sum to avoid an uncontrolled growth
-	if(sum_error > MAX_SUM_ERROR){
-		sum_error = MAX_SUM_ERROR;
-	}else if(sum_error < -MAX_SUM_ERROR){
-		sum_error = -MAX_SUM_ERROR;
+
+	speed = (KP * error) * NSTEP_ONE_TURN / WHEEL_PERIMETER;//+ KI * sum_error;
+	/* limit motor speed */
+	if (speed > MOTOR_SPEED_LIMIT) {
+	   speed = MOTOR_SPEED_LIMIT;
+	} else if (speed < -MOTOR_SPEED_LIMIT) {
+	   speed = -MOTOR_SPEED_LIMIT;
 	}
 
-	speed = KP * error ;//+ KI * sum_error;
-
-    return (int16_t)speed;
+//	chprintf((BaseSequentialStream *)&SD3,"::::::: Speed = %d:::::::", speed);
+    return  speed;
 }
 
-// Set the target position
+// Set the target position in cm
 void motor_set_position(float position_r, float position_l, float speed_r, float speed_l)
 {
 
@@ -130,49 +134,51 @@ static THD_FUNCTION(PiRegulator, arg) {
         switch (mode){
         	// PI regulator active when image processor Thread search the code
         	case SEARCH_MODE :
-        		while (mode == SEARCH_MODE)
-				{
-					//distance_mm is modified by VL53L0X sensors
-					distance = VL53L0X_get_dist_mm();
-					//chprintf((BaseSequentialStream *)&SD3,"Distance = %d", distance);
-					speed = pi_regulator(distance, GOAL_DISTANCE);
-					right_motor_set_speed(speed);
-					left_motor_set_speed(speed);
-					//Take back the mode in image processor Thread
-	        		mode = get_mode();
-				}
+				//distance_mm is modified by VL53L0X sensors
+				distance = VL53L0X_get_dist_mm()*MM_TO_CM;
+				speed = pi_regulator(distance, GOAL_DISTANCE);
+				right_motor_set_speed(speed);
+				left_motor_set_speed(speed);
+				//Take back the mode in image processor Thread
+	        	mode = get_mode();
 
 			//Execution mode : Execute the code
         	case EXE_MODE :
         		get_tab(tab_cmd);
-        		for (int i = 0; i < MAX_CODE_LENGTH; i++ ) {
-        					chprintf((BaseSequentialStream *)&SD3,"tab %d : %d - ", i, tab_cmd[i]);
-        				}
+//        		for (int i = 0; i < MAX_CODE_LENGTH; i++ ) {
+//        					chprintf((BaseSequentialStream *)&SD3,"tab %d : %d - ", i, tab_cmd[i]);
+//        				}
         		while (!stop)
         		{
         			switch (tab_cmd[i]){
 						case ADVANCE:
 							tab_cmd[i]=RETREAT;
 							motor_set_position(20, 20, 7, 7);
+							set_led(LED1,1);
 							break;
 						case RETREAT:
 							tab_cmd[i]=ADVANCE;
 							motor_set_position(20, 20, -7, -7);
+							set_led(LED5,1);
 							break;
 						case RIGHT:
 							tab_cmd[i]=LEFT;
 							motor_set_position(PERIMETER_EPUCK/4, PERIMETER_EPUCK/4, -7, 7);
+							set_led(LED3,1);
 							break;
 						case LEFT:
 							tab_cmd[i]=RIGHT;
 							motor_set_position(PERIMETER_EPUCK/4, PERIMETER_EPUCK/4, 7, -7);
+							set_led(LED7,1);
 							break;
 						case END:
 							motor_set_position(PERIMETER_EPUCK, PERIMETER_EPUCK, 7, -7);
+							set_body_led(1);
 							break;
         			}
         			while(motor_position_reached() != POSITION_REACHED);
-
+        			clear_leds();
+        			set_body_led(0);
         			//Reversing the order of reading when it's the end
         			if ((tab_cmd[i]==END) | ((i+1==MAX_CODE_LENGTH) & (!reverse))){
         				reverse=1;
